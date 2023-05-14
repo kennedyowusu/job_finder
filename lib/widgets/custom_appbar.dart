a51520c57a32648cf7de3adface2f919afd81e9b
Kennedy Owusu <kennediowusu@gmail.com>
2023-05-14 10:19:15 +0000

    add custom app bar widget

---
+++ lib/widgets/custom_appbar.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:job_finder/constants/images.dart';
+import 'package:job_finder/widgets/custom_text_style.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const CustomAppBar({Key? key}) : super(key: key);
+
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    return Container(
+      height: height * 2.5,
+      padding: EdgeInsets.only(
+        top: MediaQuery.of(context).padding.top,
+        left: 25,
+        right: 25,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 3,
+              ),
+              CustomTextStyle(
+                text: 'Welcome Home',
+                textStyle: GoogleFonts.poppins(
+                  textStyle: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 1,
+              ),
+              CustomTextStyle(
+                text: 'Kennedy Owusu',
+                textStyle: GoogleFonts.poppins(
+                  textStyle: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                margin: EdgeInsets.only(
+                  right: 10,
+                  top: 20,
+                ),
+                transform: Matrix4.rotationZ(100),
+                child: Stack(
+                  children: [
+                    Icon(
+                      Icons.notifications_none_outlined,
+                      size: 30,
+                      color: Colors.grey,
+                    ),
+                    Positioned(
+                      top: 0,
+                      right: 0,
+                      child: Container(
+                        width: 10,
+                        height: 10,
+                        padding: EdgeInsets.all(2),
+                        decoration: BoxDecoration(
+                          color: Colors.red,
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                width: width * 0.045,
+              ),
+              ClipOval(
+                child: Image.asset(
+                  PROFILEIMAGE,
+                  width: 40,
+                  height: 40,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
