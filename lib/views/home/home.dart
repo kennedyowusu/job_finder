5a143506ce106502608aad008455af4e22029fca
Kennedy Owusu <kennediowusu@gmail.com>
2023-05-14 10:41:57 +0000

    import the search field in the home file

--- lib/views/home/home.dart
+++ lib/views/home/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:job_finder/views/home/custom_search_card.dart';
 import 'package:job_finder/widgets/custom_appbar.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -10,23 +11,32 @@ class HomeScreen extends StatelessWidget {
       resizeToAvoidBottomInset: true,
       appBar: CustomAppBar(),
       body: SafeArea(
-        child: Stack(
-          children: [
-            Row(
-              children: [
-                Expanded(
-                  flex: 2,
-                  child: Container(),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: Container(
-                    color: Colors.grey.withOpacity(0.1),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Stack(
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: Container(),
                   ),
-                ),
-              ],
-            ),
-          ],
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      color: Colors.grey.withOpacity(0.1),
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  SearchCard(),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
