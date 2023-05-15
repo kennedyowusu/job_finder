a70a7f706021c4392393122897442339a04015db
Kennedy Owusu <kennediowusu@gmail.com>
2023-05-15 00:30:20 +0000

    create job list

---
+++ lib/views/home/job_list.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:job_finder/model/job.dart';
+import 'package:job_finder/widgets/job_card.dart';
+import 'package:job_finder/widgets/job_detail.dart';
+
+class JobList extends StatelessWidget {
+  JobList({super.key});
+
+  final jobList = Job.jobList();
+
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 25.0),
+      height: height * 0.25,
+      child: ListView.separated(
+        shrinkWrap: true,
+        scrollDirection: Axis.horizontal,
+        padding: EdgeInsets.symmetric(horizontal: 25.0),
+        itemCount: jobList.length,
+        separatorBuilder: (context, index) {
+          return SizedBox(width: 20.0);
+        },
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              showModalBottomSheet(
+                backgroundColor: Colors.transparent,
+                isScrollControlled: true,
+                context: context,
+                builder: (BuildContext context) => JobDetails(
+                  job: jobList[index],
+                ),
+              );
+            },
+            child: JobCard(
+              job: jobList[index],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
